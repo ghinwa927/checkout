3ead3f4a02e84b7b68e29701dc0e54a0ce7d7f7f
ghinwa927 <ghinwaabousahyoun9@gmail.com>
2026-06-10 00:27:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -187,24 +187,6 @@ payBtn.addEventListener("click", () => {
     // Confetti effect
     const duration = 2 * 1000;
     const end = Date.now() + duration;
-
-    (function frame() {
-      confetti({
-        particleCount: 5,
-        angle: 60,
-        spread: 55,
-        origin: { x: 0 }
-      });
-      confetti({
-        particleCount: 5,
-        angle: 120,
-        spread: 55,
-        origin: { x: 1 }
-      });
-      if (Date.now() < end) {
-        requestAnimationFrame(frame);
-      }
-    })();
   }, 2000);
 });
 
